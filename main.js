bdd0ce9305c449ffdf94796c1f36ed4e073c20b3
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-02-16 15:16:30 -0500

    finished

--- main.js
+++ main.js
@@ -6,8 +6,27 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (i =0; i < 5; i++) {
+    let pic = ('images/pic' + i +'.jpg')
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src',pic);
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function changePic() {
+        displayedImage.src = pic;
+    }
+}
+
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener("click", () => {
+    if (btn.getAttribute("class") === "dark") {
+      btn.setAttribute("class", "light");
+      btn.textContent = "Lighten";
+      overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    } else {
+      btn.setAttribute("class", "dark");
+      btn.textContent = "Darken";
+      overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+  });
